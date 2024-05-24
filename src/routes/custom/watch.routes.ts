1abea85b72ea7e0f050b3ee1a90dcd559b580bca
Lakshan-Chamoditha-Perera <perera.alc2000@gmail.com>
2024-05-24 14:45:57 +0530

    updated watch routes

--- src/routes/custom/watch.routes.ts
+++ src/routes/custom/watch.routes.ts
@@ -5,5 +5,7 @@ const watchRoutes: Router = Router();
 
 watchRoutes.post("/", authMiddleware,WatchController.createItem);
 watchRoutes.get("/", authMiddleware,WatchController.getItems);
+watchRoutes.get("/:id", authMiddleware,WatchController.findWatchById);
+watchRoutes.put("/:id", authMiddleware,WatchController.updateWatchById);
 
 export default watchRoutes;
